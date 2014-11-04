5ce6ed48b8fdfc2d3addd5843d6251a4bda6e09f
Danny Pranoto <danny.pranoto@veritrans.co.id>
2014-11-05 01:14:56 +0700

    Updated Purchase dropdown menu

--- app/config/database.php
+++ app/config/database.php
@@ -26,7 +26,7 @@ return array(
 	|
 	*/
 
-	'default' => 'mysql',
+	'default' => 'psql',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -66,9 +66,9 @@ return array(
 		'pgsql' => array(
 			'driver'   => 'pgsql',
 			'host'     => 'localhost',
-			'database' => 'forge',
-			'username' => 'forge',
-			'password' => '',
+			'database' => 'connect',
+			'username' => 'cyclos',
+			'password' => 'cyclos',
 			'charset'  => 'utf8',
 			'prefix'   => '',
 			'schema'   => 'public',
