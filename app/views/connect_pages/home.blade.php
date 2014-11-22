65dc2f1265b8b4d0f59cb5f6747cb8f2660e244f
Danny Pranoto <danny.pranoto@veritrans.co.id>
2014-11-22 15:21:24 +0700

    Updated Home Page

--- app/views/connect_pages/home.blade.php
+++ app/views/connect_pages/home.blade.php
@@ -1,7 +1,22 @@
 @extends('layouts.default')
 
 @section('content')
-	<div id="home-page">
-
+	<div id="home-page" class="block">
+		<div class="centered">
+			{{ HTML::image('images/slider2.png', 'Image 1', array('class' => 'centered')) }}
+			<h2>Simplifying Online Transaction</h2>
+			<h3>a reliable, easy and secure payment solution for your online store</h3>
+		</div>
 	</div>
+
+<script type="text/javascript">
+	
+	$function(){
+		
+	}
+
+</script>
+
+
 @stop
+
